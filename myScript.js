3923bc6e5d813192340459b430c02c661588935a
ossianhempel <hemposse@hotmail.com>
2022-08-23 17:22:59 +0200

    Create 16x16 grid

--- myScript.js
+++ myScript.js
@@ -1,8 +1,22 @@
-const content = document.querySelector('.content');
-console.log(content.ATTRIBUTE_NODE);
-for (let i = 0; i < 16; i++) {
-    for (let x = 0; x < 16; x++) {
+const container = document.querySelector('.container');
+
+const grid = document.createElement('div');
+grid.setAttribute('class', 'grid');
+container.appendChild(grid);
+
+// gridContainer.setAttribute('style', 'width: 360px; border: 2px solid blue; display: flex; flex-wrap: wrap; justify-content: center; box-sizing: border-box;');
+
+
+
+for (let i = 1; i <= 16; i++) {
+    for (let x = 1; x <= 16; x++) {
         const div = document.createElement('div');
-        content.appendChild(div);
+        div.setAttribute('class', 'grid-cell');
+        div.textContent = 'x';
+        grid.appendChild(div);
     }
 }
+
+
+
+// https://bluegalaxy.info/codewalk/2018/03/01/html-css-create-square-grid-using-css-grid/
